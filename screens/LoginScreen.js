a3c2d8283da5a894b21d209236324fda928dd1c5
danieljimenez0255 <42664403+danieljimenez0255@users.noreply.github.com>
2021-04-13 20:35:49 -0700

    All of the screens

---
+++ screens/LoginScreen.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from "react";
+import { KeyboardAvoidingView } from "react-native";
+import { StatusBar } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, Input, Image } from "react-native-elements";
+import { auth } from "../firebase";
+
+const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  //d.newsweek.com/en/full/520858/supermoon-moon-smartphone-photo-picture.jpg
+
+  https: useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        console.log(authUser);
+        navigation.replace("Home");
+      }
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const signIn = () => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => alert(error.message));
+  };
+
+  return (
+    <KeyboardAvoidingView behavior="padding" enabled style={styles.container}>
+      <StatusBar style="light" />
+      <Image
+        source={{
+          uri:
+            "https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png",
+        }}
+        style={{ width: 200, height: 200 }}
+      />
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Email"
+          autoFocus
+          type="email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          secureTextEntry
+          type="password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          onSubmitEditing={signIn}
+        />
+      </View>
+      <Button containerStyle={styles.button} onPress={signIn} title="Login" />
+      <Button
+        containerStyle={styles.button}
+        onPress={() => navigation.navigate("Register")}
+        type="outline"
+        title="Register"
+      />
+      <View style={{ height: 100 }} />
+    </KeyboardAvoidingView>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    backgroundColor: "white",
+  },
+  inputContainer: {
+    width: 300,
+  },
+  button: {
+    width: 200,
+    marginTop: 10,
+  },
+});
